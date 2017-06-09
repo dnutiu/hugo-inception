eb22bddf6074ac0e59600c05b91ce817b9e1def1
Denis Nuțiu <denis.nutiu@gmail.com>
2017-06-09 15:11:59 +0300

    Publishing to gh-pages

--- ghdeploy.sh
+++ ghdeploy.sh
@@ -1,25 +1,15 @@
-#!/bin/sh
-
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
-echo "Deleting old publication"
+# remove previous publication
 rm -rf public
 mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
-
-echo "Generating site"
+# clone gh-pages branch from the local repo into a repo located within "public"
+git clone .git --branch gh-pages public
+  
+# generate
 hugo
+  
+# commit the changes in the clone and push them back to the local gh-pages branch    
+cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+# publish
+git push upstream gh-pages
